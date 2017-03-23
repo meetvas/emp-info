b5eaabdf8afa0ed17494ffaf88de9cdd3afbfd0c
ksrinivas <ksrinivas@Newt-Css16.newtglobal.chn.local>
2017-03-23 18:00:53 +0530

    Updates

--- src/main/java/com/newt/app/EmpController.java
+++ src/main/java/com/newt/app/EmpController.java
@@ -3,11 +3,15 @@ package com.newt.app;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.newt.dao.EmpService;
 import com.newt.model.Employee;
+import com.newt.util.EmpUtil;
 
 @RestController
 public class EmpController {
@@ -16,18 +20,22 @@ public class EmpController {
 
 	@Autowired
 	private EmpService empService;
-	
-    @RequestMapping("/")
-    public String home() throws Exception {
+    
+    @RequestMapping(value = "/getEmployee", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<?> getEmployee(Long id) {
     	try {
-    	      Employee employee = empService.getEmp(1l);
-    	      log.debug("employee:"+employee);
-    	      return String.valueOf(employee.getFirstName());
-    	    }
-    	    catch (Exception ex) {
-    	      log.error("exception ex"+ex);
-    	      return "User not found";
-    	    }
+    	  log.debug("Request Parameter Id: "+id);
+    	  if(id==null)
+    		  throw new Exception("User is needed !!!");
+    	  Employee employee = empService.getEmp(id);
+  	      if(employee==null)
+  	    	  throw new Exception("User is not found !!!");
+  	      log.debug("Employee: "+ employee);
+  	      return EmpUtil.getOK(employee);
+  	    } catch (Exception ex) {
+  	      log.error("Exception: "+ex);
+  	      return EmpUtil.getFailed(ex.getMessage());
+  	    }
     }
 
 }
---
+++ src/main/java/com/newt/util/EmpUtil.java
@@ -0,0 +1,18 @@
+package com.newt.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class EmpUtil {
+
+	public static ResponseEntity<?> getOK(Object obj) {
+		ResponseEntity<Object> responseEntity = new ResponseEntity<Object>(obj, HttpStatus.OK);
+		return responseEntity;
+	}
+
+	public static ResponseEntity<?> getFailed(Object obj) {
+		ResponseEntity<Object> responseEntity = new ResponseEntity<Object>(obj, HttpStatus.BAD_REQUEST);
+		return responseEntity;
+	}
+
+}
