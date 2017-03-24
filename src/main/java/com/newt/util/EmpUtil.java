bf7c33a4535b14803902bc5e217497f137d543c5
ksrinivas <ksrinivas@Newt-Css16.newtglobal.chn.local>
2017-03-24 13:07:31 +0530

    Updates

--- src/main/java/com/newt/util/EmpUtil.java
+++ src/main/java/com/newt/util/EmpUtil.java
@@ -6,12 +6,14 @@ import org.springframework.http.ResponseEntity;
 public class EmpUtil {
 
 	public static ResponseEntity<?> getOK(Object obj) {
-		ResponseEntity<Object> responseEntity = new ResponseEntity<Object>(obj, HttpStatus.OK);
+		ResponseEntity<Object> responseEntity = null;
+		responseEntity = new ResponseEntity<Object>(obj, HttpStatus.OK);
 		return responseEntity;
 	}
 
 	public static ResponseEntity<?> getFailed(Object obj) {
-		ResponseEntity<Object> responseEntity = new ResponseEntity<Object>(obj, HttpStatus.BAD_REQUEST);
+		ResponseEntity<Object> responseEntity = null;
+		responseEntity = new ResponseEntity<Object>(obj, HttpStatus.BAD_REQUEST);
 		return responseEntity;
 	}
 
